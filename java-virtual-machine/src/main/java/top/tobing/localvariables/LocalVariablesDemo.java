d70e134c20ba5eea882ba9e4ddd1584a9ec410a8
Tobingindex <652916578@qq.com>
2021-08-18 20:55:22 +0800

    [Upgrade] Upgrade java-virtual-machine module.

---
+++ java-virtual-machine/src/main/java/top/tobing/localvariables/LocalVariablesDemo.java
@@ -0,0 +1,15 @@
+package top.tobing.localvariables;
+
+/**
+ * @author tobing
+ * @date 2021/8/18 19:11
+ * @description
+ */
+public class LocalVariablesDemo {
+    public static void main(String[] args) {
+        long a = 1L;
+        long b = 2L;
+        long c = a + b;
+        System.out.println(c);
+    }
+}
